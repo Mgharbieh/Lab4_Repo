19fee7a4c88f1b271b80d41b9eb80c033d37d41e
Mgharbieh <145515451+Mgharbieh@users.noreply.github.com>
2023-09-27 18:48:28 +0000

    changed loop from 15 to 14

--- Hello.java
+++ Hello.java
@@ -8,10 +8,11 @@
 //Hopefully last commit for Jenkins (part 2)
 //Hopefully last commit for Jenkins (part 3)
 //16th try for Jenkins
+//17th try for Jenkins, changed loop ro 14 tines
 
 public class Hello {
 ​  public static void main(String[] args) {
-​​    for(int i=1;i<=15;i++) {   //loop 15 times
+​​    for(int i=1;i<=14;i++) {   //loop 14 times
 ​​​      System.out.println("Hello World..."+i);
 ​​    }
       System.out.println("Done printing!");
