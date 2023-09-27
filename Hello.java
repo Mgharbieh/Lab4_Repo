a98edeb4a4771758295a066889efbfa0cc7eb547
Mgharbieh <145515451+Mgharbieh@users.noreply.github.com>
2023-09-27 18:14:42 +0000

    Changed loop to 15 times instead of 12

--- Hello.java
+++ Hello.java
@@ -2,9 +2,11 @@
 //this is a change to push in Dev_Branch
 //This is for Jenkins to dectect a new commit
 
+//Another comment so I can push to github since Jenkins fails to find changes
+
 public class Hello {
 ​  public static void main(String[] args) {
-​​    for(int i=1;i<=12;i++) {   //loop 12 times
+​​    for(int i=1;i<=15;i++) {   //loop 15 times
 ​​​      System.out.println("Hello World..."+i);
 ​​    }
 ​  }
