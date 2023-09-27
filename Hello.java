a4a04a3bd0f696a1d95b11987eb446d37efc73fa
Mgharbieh <145515451+Mgharbieh@users.noreply.github.com>
2023-09-27 18:12:35 +0000

    Changed loop form 10 to 12

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@
 
 public class Hello {
 ​  public static void main(String[] args) {
-​​    for(int i=1;i<=12;i++) {
+​​    for(int i=1;i<=12;i++) {   //loop 12 times
 ​​​      System.out.println("Hello World..."+i);
 ​​    }
 ​  }
