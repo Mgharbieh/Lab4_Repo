fb88ce61a8bd82718aedfa9df14d4220c1455d74
Mgharbieh <145515451+Mgharbieh@users.noreply.github.com>
2023-09-28 14:13:51 +0000

    Increased loop to 15 times

--- Hello.java
+++ Hello.java
@@ -2,17 +2,11 @@
 //this is a change to push in Dev_Branch
 //This is for Jenkins to dectect a new commit
 
-//Another comment so I can push to github since Jenkins fails to find changes
-//Try #9 for Jenkins
-//Hopefully last commit for Jenkins
-//Hopefully last commit for Jenkins (part 2)
-//Hopefully last commit for Jenkins (part 3)
-//16th try for Jenkins
-//17th try for Jenkins, changed loop ro 14 tines
+//Removed all old comments, final Jenkins attempt now
 
 public class Hello {
 ​  public static void main(String[] args) {
-​​    for(int i=1;i<=14;i++) {   //loop 14 times
+​​    for(int i=1;i<=15;i++) {   //loop 15 times
 ​​​      System.out.println("Hello World..."+i);
 ​​    }
       System.out.println("Done printing!");
